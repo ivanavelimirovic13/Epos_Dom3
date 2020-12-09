3a783ecc732de7484bd4db00b8668781202a5e35
Ivana <56759682+ivanavelimirovic13@users.noreply.github.com>
2020-12-09 22:37:12 +0100

    datum

--- eposDom2/jsDoc/pocetna.js
+++ eposDom2/jsDoc/pocetna.js
@@ -1,22 +1,16 @@
-  $( function() {
+$(function() {
     function runEffect() {
-      $( "#effect" ).toggle({ percent: 50 }, 500);
+        $("#effect").toggle({ percent: 50 }, 500);
     };
-    $( "#button" ).on( "click", function() {
-      runEffect();
+    $("#button").on("click", function() {
+        runEffect();
     });
-    $( "#effect" ).hide();
-    $("#kom").on("click", function(){
-      var kom = prompt("Ostavite Vas komentar");
-  if (kom != null) {
-    document.getElementById("demo").innerHTML =
-    alert("Hvala na komentaru! :)");
-  }
+    $("#effect").hide();
+    $("#kom").on("click", function() {
+        var kom = prompt("Ostavite Vas komentar");
+        if (kom != null) {
+            document.getElementById("demo").innerHTML =
+                alert("Hvala na komentaru! :)");
+        }
     });
-  } );
- 
-
- 
-  
-
-  
+});
